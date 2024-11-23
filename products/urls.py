7e657b83e788d726d7fd6a71cc53ddaed2270ec6
Keeden <145531857+keedenf@users.noreply.github.com>
2024-11-23 22:09:30 +0200

    Delete products directory

--- products/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.products, name='products'),
-    path('add_cart/<int:product_id>/', views.add_to_cart, name='add_cart'),
-    path('cart/', views.view_cart, name='cart'),
-    path('clear_cart/', views.clear_cart, name='clear_cart'),
-]
